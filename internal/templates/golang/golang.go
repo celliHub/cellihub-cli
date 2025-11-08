078374cd24c4e72d06d218d8bfa1c8c20b5303c9
Thomacelli <tonnytg@gmail.com>
2025-11-08 13:40:14 -0300

    feat: add new structure to organize templates

---
+++ internal/templates/golang/golang.go
@@ -0,0 +1,64 @@
+package golang
+
+type Template struct {
+	Category string
+	Files    []string
+	Content  map[string]string
+}
+
+var DevcontainerContent = `{
+  "name": "%PROJECT_NAME% Dev Container",
+  "build": {
+    "dockerfile": "Dockerfile"
+  },
+  "customizations": {
+    "vscode": {
+      "settings": {
+        "terminal.integrated.shell.linux": "/bin/bash"
+      },
+      "extensions": [
+        "golang.go",
+        "ms-python.python",
+        "ms-azuretools.vscode-docker",
+        "dbaeumer.vscode-eslint",
+        "ms-vscode.makefile-tools",
+        "hashicorp.terraform"
+      ]
+    }
+  },
+  "postCreateCommand": "go version && node -v && npm -v && python3 --version && terraform --version && aws --version && kubectl version --client"
+}`
+
+var DockerfileContent = `FROM golang:1.23-bullseye
+
+# Instala dependências
+RUN apt-get update && apt-get install -y \
+    unzip \
+    curl \
+    git \
+    python3 \
+    python3-pip \
+    nodejs \
+    npm \
+    awscli \
+    && rm -rf /var/lib/apt/lists/*
+
+# Instala Terraform
+RUN curl -fsSL https://releases.hashicorp.com/terraform/1.9.5/terraform_1.9.5_linux_amd64.zip -o terraform.zip && \
+    unzip terraform.zip && mv terraform /usr/local/bin/ && rm terraform.zip
+
+# Instala Kubectl
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
+
+WORKDIR /workspace
+`
+
+var GolangTemplate = Template{
+	Category: "cloud",
+	Files:    []string{"devcontainer.json", "Dockerfile"},
+	Content: map[string]string{
+		"devcontainer.json": DevcontainerContent,
+		"Dockerfile":        DockerfileContent,
+	},
+}
